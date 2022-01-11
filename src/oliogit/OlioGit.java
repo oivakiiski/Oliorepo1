f5198f03d09bb0a96d8994a70b01781fa1751bae
oivak <oivak@192.168.1.157>
2022-01-11 14:42:23 +0200

    Initial commit

---
+++ src/oliogit/OlioGit.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package oliogit;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author oivak
+ */
+public class OlioGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hei vaan hei.");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Press enter to continue yes?");
+        scan.nextLine();
+    }
+    
+}
